4a1ae1d040662b3f29940e3bde9f285ca2e0a6c8
Alexandros Bantis <abantis@apple.com>
2015-04-18 18:07:34 -0700

    knight position generator

--- src/test/scala/com/okosmos/camino/domain/KnightPositionCheck.scala
+++ src/test/scala/com/okosmos/camino/domain/KnightPositionCheck.scala
@@ -1,12 +1,24 @@
 package com.okosmos.camino.domain
 
 import com.okosmos.camino.FunSpecChecker
+import org.scalacheck.Gen
 
-class KnightPositionCheck extends FunSpecChecker {
+class KnightPositionCheck extends FunSpecChecker with KnightPositionGenerator {
 
   describe("a knight") {
     it("should only make legal knight moves") (pending)
     it("should always make 8 moves from a given position") (pending)
   }
 
+}
+
+trait KnightPositionGenerator {
+
+  private val coordinateGen = Gen.chooseNum(Int.MinValue + 2, Int.MaxValue - 2)
+
+  val knightPosGen =
+    for {
+      x <- coordinateGen
+      y <- coordinateGen
+    } yield KnightPosition(x, y)
 }
