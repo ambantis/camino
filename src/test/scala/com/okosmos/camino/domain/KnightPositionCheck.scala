8bc904408a8370083d4ff918bfc67f6a10834d13
Alexandros Bantis <abantis@apple.com>
2015-04-18 18:38:02 -0700

    knight position checker passes

--- src/test/scala/com/okosmos/camino/domain/KnightPositionCheck.scala
+++ src/test/scala/com/okosmos/camino/domain/KnightPositionCheck.scala
@@ -1,13 +1,27 @@
 package com.okosmos.camino.domain
 
 import com.okosmos.camino.FunSpecChecker
-import org.scalacheck.Gen
+import org.scalacheck.{Arbitrary, Gen}
 
 class KnightPositionCheck extends FunSpecChecker with KnightPositionGenerator {
 
   describe("a knight") {
-    it("should only make legal knight moves") (pending)
-    it("should always make 8 moves from a given position") (pending)
+    it("should always make 8 moves from a given position") {
+      forAll() { (pos: KnightPosition) =>
+        pos.nextMoves should have size 8
+      }
+    }
+    it("should only make legal knight moves") {
+      forAll() { (pos: KnightPosition) =>
+        pos.nextMoves forall { deltaPos =>
+          (pos, deltaPos) match {
+            case (p1, p2) if math.abs(p1.x - p2.x) == 1 => math.abs(p1.y - p2.y) == 2
+            case (p1, p2) if math.abs(p1.x - p2.x) == 2 => math.abs(p1.y - p2.y) == 1
+            case _ => false
+          }
+        }
+      }
+    }
   }
 
 }
@@ -16,9 +30,10 @@ trait KnightPositionGenerator {
 
   private val coordinateGen = Gen.chooseNum(Int.MinValue + 2, Int.MaxValue - 2)
 
-  val knightPosGen =
+  implicit val arbKnightPosition: Arbitrary[KnightPosition] = Arbitrary {
     for {
       x <- coordinateGen
       y <- coordinateGen
     } yield KnightPosition(x, y)
+  }
 }
