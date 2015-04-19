d9192d405982d5f35c5ccf0a990aef9f6668576f
Alexandros Bantis <abantis@apple.com>
2015-04-18 17:44:16 -0700

    added unimplemented knight position tests

---
+++ src/test/scala/com/okosmos/camino/domain/KnightPositionCheck.scala
@@ -0,0 +1,12 @@
+package com.okosmos.camino.domain
+
+import com.okosmos.camino.FunSpecChecker
+
+class KnightPositionCheck extends FunSpecChecker {
+
+  describe("a knight") {
+    it("should only make legal knight moves") (pending)
+    it("should always make 8 moves from a given position") (pending)
+  }
+
+}
