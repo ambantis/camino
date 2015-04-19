9e545c1b717a3978d2dd980bbbafd5a67c3b4d68
Alexandros Bantis <abantis@apple.com>
2015-04-18 18:20:13 -0700

    changed Checkers to GeneratorDrivenPropertyChecks

--- src/test/scala/com/okosmos/camino/FunSpecChecker.scala
+++ src/test/scala/com/okosmos/camino/FunSpecChecker.scala
@@ -1,6 +1,6 @@
 package com.okosmos.camino
 
-import org.scalatest.prop.Checkers
+import org.scalatest.prop.{GeneratorDrivenPropertyChecks, Checkers}
 import org.scalatest.{FunSpec, Matchers}
 
-trait FunSpecChecker extends FunSpec with Matchers with Checkers
+trait FunSpecChecker extends FunSpec with Matchers with GeneratorDrivenPropertyChecks
