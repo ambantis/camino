780b1306673ecb0512fadf7d3e3c9dc02be07b7d
Alexandros Bantis <abantis@apple.com>
2015-04-19 17:26:58 -0700

    minor improvements

--- src/main/scala/com/okosmos/camino/service/Worker.scala
+++ src/main/scala/com/okosmos/camino/service/Worker.scala
@@ -1,16 +1,24 @@
 package com.okosmos.camino.service
 
-import akka.actor.{ActorRef, ActorLogging, Actor}
+import akka.actor.{Props, ActorLogging, Actor}
 import akka.dispatch.RequiresMessageQueue
 import com.okosmos.camino.domain.Board
+import com.okosmos.camino.service.SolutionFinder.BoardSolution
 
-class Worker(collector: ActorRef) extends Actor with ActorLogging with RequiresMessageQueue[ChessBoardMailbox] {
+object Worker {
+  def name(n: Int) = s"worker-num-$n"
+  def props = Props(classOf[Worker]).withDispatcher("prio-dispatcher")//.withMailbox("worker-mailbox")
+}
+
+class Worker extends Actor with ActorLogging /*with RequiresMessageQueue[ChessBoardMailbox]*/ {
 
   import context.parent
   override def receive = {
     case b: Board =>
       val (solved, unsolved) = b.next partition(_.isDone)
-      solved foreach (collector ! _)
+      log.info(s"processing ${unsolved.size} unsolved with priorities: ${unsolved.map(_.remaining).mkString(",")}")
+      log.info(s"FOUND SOLUTIONS: $solved")
+      solved foreach (s => sender().tell(BoardSolution(s.solution), parent))
       unsolved foreach (parent ! _)
   }
 }
